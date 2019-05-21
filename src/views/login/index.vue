cfcfcc20b11d73a276650a758b7cc27d6bafef03
liwan <liwan@houyicloud.com>
2019-05-21 16:34:04 +0800

    增加请求header头 增加本地存储 修改配置文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,39 +1,36 @@
 <template>
-  <div>
-    <label for="loginName">
-      用户名：
-      <input v-model="loginName" type="text" id="loginName" />
-    </label>
-    <label for="passWord">
-      密码：
-      <input v-model="passWord" type="password" id="passWord" />
-    </label>
-    <button type="submit" @click="login">登录</button>
-  </div>
+    <div>
+        <label for="loginName">
+            用户名：
+            <input v-model="loginName" type="text" id="loginName" />
+        </label>
+        <label for="passWord">
+            密码：
+            <input v-model="passWord" type="password" id="passWord" />
+        </label>
+        <button type="submit" @click="login">登录</button>
+    </div>
 </template>
-
 <script>
-  export default {
+export default {
     name: "login",
     data() {
-      return {
-        loginName: "",
-        passWord: ""
-      }
+        return {
+            loginName: "18616876125",
+            passWord: "123456"
+        }
     },
     methods: {
-      login() {
-        this.$axios.post('/users/login', {
-          loginName: this.loginName,
-          passWord: this.passWord
-        }).then(res => {
-          console.log(res.data)
-        })
-      }
+        login() {
+            this.$axios.post('/users/login', {
+                loginName: this.loginName,
+                passWord: this.passWord
+            }).then(res => {
+                
+            })
+        }
     }
-  }
+}
 </script>
-
 <style scoped>
-
 </style>

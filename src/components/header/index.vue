51b27d2b7ba5fcbcdce161a05179512cc576afdf
Lokep <iamlokep@163.com>
2019-05-22 14:30:40 +0800

    lokep

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -8,19 +8,41 @@
 .logo-wrapper{
 	margin-right: 20px;
 }
-.navs{
-	padding:25px  0 20px;
+.el-menu,.search,.user{
+	padding:20px  0 20px;
 }
-.navs li {
-	float: left;
-	margin-right: 10px;
-	padding-bottom: 5px;
+.search{
+	background-image: url(../../assets/images/home/nav/menu.png);
+	background-repeat: no-repeat;
+	background-position: right center;
+	padding-right: 30px;
+	margin-right: 30px;
 }
-.navs .nav-active{
-	border-bottom: 2px solid skyblue;
-}
+
 .el-menu.el-menu--horizontal{
 	border-bottom: 0;
+}
+.el-menu--horizontal>.el-menu-item{
+	height: 35px;
+    line-height: 35px;
+}
+.el-menu--horizontal>.el-menu-item.is-active{
+	color:#409EFF;
+}
+
+.icon{
+	display: inline-block;
+	margin-left:20px;
+}
+.username{
+	color:#409EFF;
+	margin-bottom: 5px;
+}
+.userlevel>*{
+	vertical-align: top;
+	display:inline-block;
+	font-size: 12px;
+	line-height: 15px;
 }
 </style>
 <template>
@@ -38,18 +60,25 @@
 				<el-menu-item index="/activities">活动</el-menu-item>
 			</el-menu>
 
-			<div class="search fl">
-				<div class="search-ipt">
-					<input type="text">
-				</div>
-				<i class="icon"></i>
+			<div class="search fl ov">
+				<el-input class="fl" prefix-icon="el-icon-search" size="small" placeholder="请输入您要搜索的关键字" v-model="keywords"></el-input>
+				
 			</div>
-			<div class="user fl ov">
-				<router-link to="" tag=""></router-link>
-				<router-link to=""></router-link>
-				<router-link to=""></router-link>
-				<router-link to=""></router-link>
-				<span class=""></span>
+			<div class="user fr ov">
+				<router-link to="" class="ov">
+					<img src="../../assets/images/home/nav/head_img.png" class="fl" alt="">
+					<div class="fl">
+						<h4 class="username">这是用户名</h4>
+						<div class="userlevel">
+							<img src="../../assets/images/home/nav/level_8.png" alt="" class="inline">
+							<span class="inline">经验：2328</span>
+						</div>
+					</div>
+				</router-link>
+				<router-link to=""><img src="../../assets/images/home/nav/chat.png" alt="" class="icon"></router-link>
+				<router-link to=""><img src="../../assets/images/home/nav/friend.png" alt="" class="icon"></router-link>
+				<router-link to=""><img src="../../assets/images/home/nav/setting.png" alt="" class="icon"></router-link>
+				<img src="../../assets/images/home/nav/quit.png" alt="" class="icon">
 			</div>
 		</div>
 	</div>
@@ -57,6 +86,11 @@
 
 <script>
 	export default {
-		name: "headerWrapper"
+		name: "headerWrapper",
+		data(){
+			return{
+				keywords:''
+			}
+		}
 	}
 </script>

16baa128e4eb931ec9dc2ed0330ba553db236897
Lokep <iamlokep@163.com>
2019-05-27 17:54:26 +0800

    change the router mode
    history => hash

--- src/router.js
+++ src/router.js
@@ -41,98 +41,97 @@ let limitLogin = (to, from, next) => { //登陆限制
 };
 
 const routes = new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [{
-    path: '/',
-    component: Default,
-    children: [
-      {
-        path: '',
-        name: 'home',
-        component: () =>
-          import ('./views/home/index.vue'),
-      }, {
-        path: 'news',
-        name: 'news',
-        component: () =>
-          import ('./views/home/news.vue')
-      }, {
-        path: 'news/:articleID',
-        name: 'newsArticle',
-        component: () =>
-          import ('./views/home/article.vue')
-      }, {
-        path: 'topic',
-        name: 'topic',
-        component: () =>
-          import ('./views/home/topic.vue')
-      }, {
-        path: 'activity',
-        name: 'activity',
-        component: () =>
-          import ('./views/home/activities.vue')
-      }, {
-        path: 'activity/:articleID',
-        name: 'activityArticle',
-        component: () =>
-          import ('./views/home/article.vue')
-      }, {
-        path: 'user',
-        component: Wrapper,
-        children: [
-          {
-            path: '',
-            name: 'user',
-            component: () => import('./views/user/index.vue'),
-          },
-          {
-            path: 'editProfile',
-            name: 'editProfile',
-            component: () => import('./views/user/editProfile.vue')
-          }
-        ]
-      }
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/',
+            component: Default,
+            children: [{
+                path: '',
+                name: 'home',
+                component: () =>
+                    import ('./views/home/index.vue'),
+            }, {
+                path: 'news',
+                name: 'news',
+                component: () =>
+                    import ('./views/home/news.vue')
+            }, {
+                path: 'news/:articleID',
+                name: 'newsArticle',
+                component: () =>
+                    import ('./views/home/article.vue')
+            }, {
+                path: 'topic',
+                name: 'topic',
+                component: () =>
+                    import ('./views/home/topic.vue')
+            }, {
+                path: 'activity',
+                name: 'activity',
+                component: () =>
+                    import ('./views/home/activities.vue')
+            }, {
+                path: 'activity/:articleID',
+                name: 'activityArticle',
+                component: () =>
+                    import ('./views/home/article.vue')
+            }, {
+                path: 'user',
+                component: Wrapper,
+                children: [{
+                        path: '',
+                        name: 'user',
+                        component: () =>
+                            import ('./views/user/index.vue'),
+                    },
+                    {
+                        path: 'editProfile',
+                        name: 'editProfile',
+                        component: () =>
+                            import ('./views/user/editProfile.vue')
+                    }
+                ]
+            }]
+        },
+        {
+            path: '/',
+            component: noFooter,
+            children: [{
+                path: '/login',
+                name: 'login',
+                beforeEnter: limitNotLogin,
+                component: () =>
+                    import ('./views/user/login.vue')
+            }, {
+                path: '/register',
+                name: 'register',
+                beforeEnter: limitNotLogin,
+                component: () =>
+                    import ('./views/user/register.vue')
+            }, {
+                path: '/bound',
+                name: 'bound',
+                component: () =>
+                    import ('./views/user/bound.vue')
+            }, {
+                path: '/findPassword',
+                name: 'findPassword',
+                component: () =>
+                    import ('./views/user/findPassword.vue')
+            }]
+        },
+        {
+            path: '/',
+            component: bbsHeader,
+            children: [{
+                path: '/bbs',
+                name: 'bbs',
+                component: () =>
+                    import ('./views/bbs/index.vue')
+            }]
+        }
     ]
-  },
-    {
-      path: '/',
-      component: noFooter,
-      children: [{
-        path: '/login',
-        name: 'login',
-        beforeEnter: limitNotLogin,
-        component: () =>
-          import ('./views/user/login.vue')
-      },{
-        path: '/register',
-        name: 'register',
-        beforeEnter: limitNotLogin,
-        component: () =>
-          import ('./views/user/register.vue')
-      },{
-        path: '/bound',
-        name: 'bound',
-        component: () =>
-          import ('./views/user/bound.vue')
-      },{
-        path: '/findPassword',
-        name: 'findPassword',
-        component: () =>
-          import ('./views/user/findPassword.vue')
-      }]
-    },
-    {
-      path: '/',
-      component: bbsHeader,
-      children: [{
-        path: '/bbs',
-        name: 'bbs',
-        component: () =>
-          import('./views/bbs/index.vue')
-      }]
-    }
-  ]
 });
 
 export default routes;
